6f689b8767526959471b7a98c834110835d1e45f
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-06-05 13:16:52 -0300

    PHP
    Testando as configurações de PHP junto com as correções de localização do xammpp em meu computador

---
+++ hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu PHP Esta no AR</title>
+</head>
+<body>
+    <?php
+        echo "<p>Vai Toma</p>";
+    ?>
+</body>
+</html>
