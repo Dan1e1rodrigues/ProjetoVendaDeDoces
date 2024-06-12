90abc48b7ac8137d9e32156d0953222f79985d76
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-06-12 15:54:43 -0300

    tela de sobre
    Inicializando a tela de sobre, com a apresentação e portifólio da cliente.

--- principal/perfilSobre.php
+++ principal/perfilSobre.php
@@ -2,189 +2,130 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Carla, Leo, Victoria">
+    <meta name="description" content="Pagina inicial Home">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/perfilSobre.css">
-    <title>Um pouco sobre mim</title>
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"> -->
-    <!-- Inclua a biblioteca P5.js -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script> -->
+    <link rel="stylesheet" href="./css/perfilSobre.css">
+    <title>Pagina de portifolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body>
 
   <header>
     <div class="logo">
-        <img src="./images/imagemVazia.jpg" alt="Logotipo da Minha Empresa" id="logo">
+        <img src="./images/logo/Black & White Minimalist Aesthetic Initials Font Logo.jpg" alt="Logotipo da Minha Empresa" id="logo">
     </div>
     <nav>
-        <a href="../HTML/ARQUIVO.HTML">Início</a>
-        <a href="../HTML/portifolioC.html">Carla</a>
-        <a href="../HTML/portifolioL.html">Leonardo</a>
-        <a href="../HTML/portifolioV.html">Victória</a>
-        <a href="../HTML/login.html">Login</a>
+        <a href="/ProjetoVendaDeDoces/principal/">Início</a>
+        <a href="../HTML/portifolioC.html">Produtos</a>
+        <a href="../HTML/portifolioL.html">Seus Pedidos</a>
+        <a href="../HTML/portifolioV.html">Faça sua encomenda</a>
         <div class="header-buttons">
-          <a class="contact-button" href="#footer">Fala com a gente</a>
-            <img src="/principal/images/imagemVazia.jpg" alt="Logo Redonda" class="logo-redonda">
+          <a class="contact-button" href="/ProjetoVendaDeDoces/principal/login.php">Login</a>
+          <a class="contact-button" href="/ProjetoVendaDeDoces/principal/register.php">Cadastro</a>
+          <img src="./images/logo/Black & White Minimalist Aesthetic Initials Font Logo.jpg" alt="" class="logo-redonda">
         </div>
     </nav>
-</header>
-
-    <!-- Banner simples -->
-    <!-- <div class="banner">
-      <img src="" alt="Banner Image">
-    </div> -->
-
+  </header>
 
     <div class="meu-retangulo">
       <div class="imagem-container">
           <img src="./images/imagemVazia.jpg" alt="Minha Imagem">
       </div>
       <div class="texto-container">
-          <p>"O epicentro da inovação e da narrativa visual, onde três colegas do Senac Santo Amaro convergem suas paixões e habilidades. Neste espaço virtual, testemunhe nossa jornada educacional transformada em criações únicas e envolventes."</p>
+          <p>"Epicentro da inovação e da narrativa visual, onde três colegas do Senac Santo Amaro convergem suas paixões e habilidades. Neste espaço virtual, testemunhe nossa jornada educacional transformada em criações únicas e envolventes."</p>
           <p>Carla, Victória e Leonardo.</p>
       </div>
     </div>
 
-    <section id="Sobre-info" class="centered-container">
-        <div class="content">
-            <p style="color: #F87509;">DESIGNER UX/UI</p>
-            <h2 class="justified"><strong>Olá, meu nome é Carla Edwirges</strong></h2>
-            <p>Sou uma designer apaixonada por criar soluções inovadoras e centralizadas no usuário. Minha formação é em arquitetura e urbanismo, mas atualmente estou cursando Design no Centro Universitário SENAC e estou adorando esse novo desafio.</p>
-            <div class="button-container">
-                <a href="../HTML/portifolioC.html" class="button yellow-button">Saiba Mais</a>
-                <a href="https://www.linkedin.com/in/carlamendessantos-designux/" class= "button linkedin-button" style="background-color: #fff; border: solid yellow;">LinkedIn</a>
-            </div>            
-        </div>
-        <img src="./images/imagemVazia.jpg" alt="Carla Perfil" class="profile-image">
-    </section>
+<section id="Sobre-info" class="centered-container">
+    <div class="content">
+        <p class="highlight">DESIGNER UX/UI</p>
+        <h2 class="justified"><strong>Olá, meu nome é Carla Edwirges</strong></h2>
+        <p>Sou uma designer apaixonada por criar soluções inovadoras e centralizadas no usuário. Minha formação é em arquitetura e urbanismo, mas atualmente estou cursando Design no Centro Universitário SENAC e estou adorando esse novo desafio.</p>
+        <div class="button-container">
+            <a href="" class="button yellow-button">Saiba Mais</a>
+            <a href="" class="button linkedin-button">LinkedIn</a>
+        </div>            
+    </div>
+    <img src="./images/imagemVazia.jpg" alt="Carla Perfil" class="profile-image">
+</section>
 
     <div class="text-center">
-      <h2 style="color: black; margin: 20px 0;">Projetos destaque do semestre</h2>
+      <h2 class="title">Projetos destaque do semestre</h2>
       <div class="yellow-line"></div>
     </div>
 
-    <div class="card-container">
-        <div class="card">
-          <div class="card-header"></div>
-          <div class="card-body">
-            <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 1">
-            <h2 style="color: #F87509;"><a href="./images/imagemVazia.jpg">Monitoramento completo da saúde</a></h2>
-            <p>Projeto Inovador (5° Semestre).
-            Pensar no bem estar do idoso 24h por dia.
-            avaliando a saúde e o seu dia a dia.</p>
-          </div>
-        </div>
-      
-        <div class="card">
-          <div class="card-header"></div>
-          <div class="card-body">
-            <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 2">
-            <h2 style="color: #F87509;"><a href="./images/imagemVazia.jpg">Rota segura</a></h2>
-            <p>Projeto desenvolvi nas aulas recorrentes, 
-              pensar nos desafios da mobilidade 
-              urbana em tempos de chuva</p>
-          </div>
-        </div>
+<div class="card-container">
+    <div class="card">
+      <div class="card-body">
+        <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 1">
+        <h2 class="highlight"><a href="../HTML/carlaPmonitoramento.html">Monitoramento completo da saúde</a></h2>
+        <p>Projeto Inovador (5° Semestre). Pensar no bem estar do idoso 24h por dia, avaliando a saúde e o seu dia a dia.</p>
       </div>
-    <hr>
-
-    <section id="Outra-secao" class="centered-container">
-        <div class="content">
-            <div>
-                <p style="color: #F87509;">DESIGNER</p>
-                <h2 class="justified">Olá, meu nome é Leonardo</h2>
-                <p>Atualmente estou 2° semestre do curso de Design. Minha trajetória acadêmica começou em Análise e Desenvolvimento de Sistemas (ADS), mas foi transição para Design que descobri verdadeiramente minha paixão pelo design e por suas infinitas possibilidades de criação.</p>
-                <div class="button-container">
-                    <a href="./images/imagemVazia.jpg" class="button yellow-button">Saiba Mais</a>
-                    <a href="https://www.linkedin.com/in/leonardo-costa-de-moraes/" class= "button linkedin-button" style="background-color: #fff; border: solid yellow;">LinkedIn</a>
-                </div>  
-            </div>            
-        </div>
-        <img src="./images/imagemVazia.jpg" alt="Imagem Leonardo">
-    </section>
-
-    <div class="text-center">
-      <h2 style="color: black; margin: 20px 0;">Projetos destaque do semestre</h2>
-      <div class="yellow-line"></div>
     </div>
-
-    <div class="card-container">
-        <div class="card">
-          <div class="card-header"></div>
-          <div class="card-body">
-            <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 3">
-            <h2 style="color: #F87509;">Desenvolvimento Mobile para iOS</h2>
-            <p>Projeto desenvolvido no 2° semestre para dispositivos iOS. Utilizando Swift e explorando ferramentas nativas da Apple para oferecer uma experiência única aos usuários.</p>
-          </div>
-        </div>
-    </div>
-    <hr>
-
-    <section id="Outra-secao" class="centered-container">
-        <div class="content">
-            <div>
-                <p style="color: #F87509;">DESIGNER</p>
-                <h2 class="justified">Olá, meu nome é Victória França</h2>
-                <p>Atualmente no 2° semestre de Design, sempre fui apaixonada por arte e tecnologia. Meu objetivo é unir esses dois mundos para criar experiências visuais únicas e envolventes.</p>
-                <div class="button-container">
-                    <a href="./images/imagemVazia.jpg" class="button yellow-button">Saiba Mais</a>
-                    <a href="https://www.linkedin.com/in/victoria-franca/" class= "button linkedin-button" style="background-color: #fff; border: solid yellow;">LinkedIn</a>
-                </div>  
-            </div>            
-        </div>
-        <img src="./images/imagemVazia.jpg" alt="Imagem Victória">
-    </section>
-
-    <div class="text-center">
-      <h2 style="color: black; margin: 20px 0;">Projetos destaque do semestre</h2>
-      <div class="yellow-line"></div>
-    </div>
-
-    <div class="card-container">
-        <div class="card">
-          <div class="card-header"></div>
-          <div class="card-body">
-            <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 4">
-            <h2 style="color: #F87509;">Design Gráfico Avançado</h2>
-            <p>Exploração de técnicas avançadas de design gráfico para criar identidades visuais fortes e memoráveis.</p>
-          </div>
-        </div>
-      
-        <div class="card">
-          <div class="card-header"></div>
-          <div class="card-body">
-            <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 5">
-            <h2 style="color: #F87509;">Projeto de Animação 3D</h2>
-            <p>Criação de animações 3D utilizando Blender, com foco em narrativa visual e efeitos especiais.</p>
-          </div>
-        </div>
+  
+    <div class="card">
+      <div class="card-body">
+        <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 2">
+        <h2 class="highlight"><a href="../HTML/carlaRotaSegura.html">Rota segura</a></h2>
+        <p>Projeto desenvolvido nas aulas recorrentes, pensando nos desafios da mobilidade urbana em tempos de chuva.</p>
       </div>
-    <hr>
+    </div>
+</div>
 
-  <footer id="footer">
-    <div class="footer-container">
-        <div class="contact-info">
-            <h3>Contato</h3>
-            <p>Nome: Carla Edwirges</p>
-            <p>Telefone: (11) 99999-9999</p>
-            <p>Email: carlaedwirges@example.com</p>
-        </div>
-        <div class="contact-info">
-            <h3>Contato</h3>
-            <p>Nome: Leonardo Costa de Moraes</p>
-            <p>Telefone: (11) 88888-8888</p>
-            <p>Email: leonardocosta@example.com</p>
-        </div>
-        <div class="contact-info">
-            <h3>Contato</h3>
-            <p>Nome: Victória França</p>
-            <p>Telefone: (11) 77777-7777</p>
-            <p>Email: victoriafranca@example.com</p>
-        </div>
-        <div class="footer-logo">
-            <img src="./images/imagemVazia.jpg" alt="Logo Footer">
-            <p>Desenvolvido por Carla, Leonardo e Victória.</p>
+<hr>
+
+<div id="footer"></div>
+
+<footer class="main-footer">
+    <div class="container">
+        <div class="footer-content">
+            <div class="section about-institution">
+                <img src="../ASSETS/LOGO/Logo footer.png" alt="" id="header-image">
+                <p class="about-text">Ter um portfólio de design é fundamental para os profissionais da área, pois é uma ferramenta poderosa para demonstrar suas habilidades, experiência e estilo de design. É uma vitrine virtual que permite mostrar nossos trabalhos para potenciais clientes, empregadores ou parceiros de negócios.</p>
+            </div>
+            <div class="section contact-info">
+                <h3 class="footer-title">Contato</h3>
+                <ul>
+                    <li>
+                        <p class="highlight"><strong>Victoria França</strong></p>
+                        <p>(11) 91234-5678</p>
+                        <p>victoria@senac.com.br</p>
+                    </li>
+                    <li>
+                        <p class="highlight"><strong>Carla Santos</strong></p>
+                        <p>(11) 91234-5679</p>
+                        <p>carla@senac.com.br</p>
+                    </li>
+                    <li>
+                        <p class="highlight"><strong>Leonardo</strong></p>
+                        <p>(11) 91234-5680</p>
+                        <p>leonardo@senac.com.br</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="section social-media">
+                <h3 class="footer-title">Redes Sociais</h3>
+                <ul class="social-icons">
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/victoria-oliveira-frança-baa0b3a1/"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="mailto:victoria@senac.com.br"><i class="fas fa-envelope"></i></a></li>
+                </ul>
+                <ul class="social-icons">
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/carlamendessantos-designux/"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="mailto:carlasantos.educ@gmail.com"><i class="fas fa-envelope"></i></a></li>
+                </ul>
+                <ul class="social-icons">
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/leonardocostademoraes/"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="mailto:leonardo@senac.com.br"><i class="fas fa-envelope"></i></a></li>
+                </ul>
+            </div>
         </div>
     </div>
-  </footer>
+</footer>
 </body>
 </html>
