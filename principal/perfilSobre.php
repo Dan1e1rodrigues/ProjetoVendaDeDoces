d9331e823dd9e49fe037f6cd072efdbca64679cb
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-06-12 21:42:33 -0300

    pagina de portifolio

--- principal/perfilSobre.php
+++ principal/perfilSobre.php
@@ -1,131 +1,45 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="robots" content="index, follow">
-    <meta name="author" content="Carla, Leo, Victoria">
-    <meta name="description" content="Pagina inicial Home">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <title>Um pouco da Minha Historia</title>
     <link rel="stylesheet" href="./css/perfilSobre.css">
-    <title>Pagina de portifolio</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body>
+    <header id="header">
+        <div class="container">
 
-  <header>
-    <div class="logo">
-        <img src="./images/logo/Black & White Minimalist Aesthetic Initials Font Logo.jpg" alt="Logotipo da Minha Empresa" id="logo">
-    </div>
-    <nav>
-        <a href="/ProjetoVendaDeDoces/principal/">Início</a>
-        <a href="../HTML/portifolioC.html">Produtos</a>
-        <a href="../HTML/portifolioL.html">Seus Pedidos</a>
-        <a href="../HTML/portifolioV.html">Faça sua encomenda</a>
-        <div class="header-buttons">
-          <a class="contact-button" href="/ProjetoVendaDeDoces/principal/login.php">Login</a>
-          <a class="contact-button" href="/ProjetoVendaDeDoces/principal/register.php">Cadastro</a>
-          <img src="./images/logo/Black & White Minimalist Aesthetic Initials Font Logo.jpg" alt="" class="logo-redonda">
-        </div>
-    </nav>
-  </header>
+            <div class="flex">
 
-    <div class="meu-retangulo">
-      <div class="imagem-container">
-          <img src="./images/imagemVazia.jpg" alt="Minha Imagem">
-      </div>
-      <div class="texto-container">
-          <p>"Epicentro da inovação e da narrativa visual, onde três colegas do Senac Santo Amaro convergem suas paixões e habilidades. Neste espaço virtual, testemunhe nossa jornada educacional transformada em criações únicas e envolventes."</p>
-          <p>Carla, Victória e Leonardo.</p>
-      </div>
-    </div>
+                <a href="#"><i class="bi bi-globe-americas"></i></a>
 
-<section id="Sobre-info" class="centered-container">
-    <div class="content">
-        <p class="highlight">DESIGNER UX/UI</p>
-        <h2 class="justified"><strong>Olá, meu nome é Carla Edwirges</strong></h2>
-        <p>Sou uma designer apaixonada por criar soluções inovadoras e centralizadas no usuário. Minha formação é em arquitetura e urbanismo, mas atualmente estou cursando Design no Centro Universitário SENAC e estou adorando esse novo desafio.</p>
-        <div class="button-container">
-            <a href="" class="button yellow-button">Saiba Mais</a>
-            <a href="" class="button linkedin-button">LinkedIn</a>
-        </div>            
-    </div>
-    <img src="./images/imagemVazia.jpg" alt="Carla Perfil" class="profile-image">
-</section>
+                <nav>
+                    <ul>
+                        <li><a href="#">MEUS PRODUTOS</a></li>
+                        <li><a href="#">REDES SOCIAIS</a></li>
+                        <li><a href="#">FAÇA SEU PEDIDO</a></li>
+                        <li><a href="#">BLOG</a></li>
+                    </ul>
+                </nav>
 
-    <div class="text-center">
-      <h2 class="title">Projetos destaque do semestre</h2>
-      <div class="yellow-line"></div>
-    </div>
+                <div class="btn-contato">
+                    <a href="/ProjetoVendaDeDoces/principal/"><button>VAMOS PARA LOJA</button></a>
+                </div>
 
-<div class="card-container">
-    <div class="card">
-      <div class="card-body">
-        <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 1">
-        <h2 class="highlight"><a href="../HTML/carlaPmonitoramento.html">Monitoramento completo da saúde</a></h2>
-        <p>Projeto Inovador (5° Semestre). Pensar no bem estar do idoso 24h por dia, avaliando a saúde e o seu dia a dia.</p>
-      </div>
-    </div>
-  
-    <div class="card">
-      <div class="card-body">
-        <img src="./images/imagemVazia.jpg" alt="Descrição da imagem 2">
-        <h2 class="highlight"><a href="../HTML/carlaRotaSegura.html">Rota segura</a></h2>
-        <p>Projeto desenvolvido nas aulas recorrentes, pensando nos desafios da mobilidade urbana em tempos de chuva.</p>
-      </div>
-    </div>
-</div>
+            </div><!--flex-->
 
-<hr>
+        </div><!--container-->
+    </header>
 
-<div id="footer"></div>
+    <section class="banner">
+        <!-- <h1>DESCUBRA UM <span>NOVO MUNDO</span></h1> -->
+    </section>
 
-<footer class="main-footer">
-    <div class="container">
-        <div class="footer-content">
-            <div class="section about-institution">
-                <img src="../ASSETS/LOGO/Logo footer.png" alt="" id="header-image">
-                <p class="about-text">Ter um portfólio de design é fundamental para os profissionais da área, pois é uma ferramenta poderosa para demonstrar suas habilidades, experiência e estilo de design. É uma vitrine virtual que permite mostrar nossos trabalhos para potenciais clientes, empregadores ou parceiros de negócios.</p>
-            </div>
-            <div class="section contact-info">
-                <h3 class="footer-title">Contato</h3>
-                <ul>
-                    <li>
-                        <p class="highlight"><strong>Victoria França</strong></p>
-                        <p>(11) 91234-5678</p>
-                        <p>victoria@senac.com.br</p>
-                    </li>
-                    <li>
-                        <p class="highlight"><strong>Carla Santos</strong></p>
-                        <p>(11) 91234-5679</p>
-                        <p>carla@senac.com.br</p>
-                    </li>
-                    <li>
-                        <p class="highlight"><strong>Leonardo</strong></p>
-                        <p>(11) 91234-5680</p>
-                        <p>leonardo@senac.com.br</p>
-                    </li>
-                </ul>
-            </div>
-            <div class="section social-media">
-                <h3 class="footer-title">Redes Sociais</h3>
-                <ul class="social-icons">
-                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/victoria-oliveira-frança-baa0b3a1/"><i class="fab fa-linkedin"></i></a></li>
-                    <li><a href="mailto:victoria@senac.com.br"><i class="fas fa-envelope"></i></a></li>
-                </ul>
-                <ul class="social-icons">
-                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/carlamendessantos-designux/"><i class="fab fa-linkedin"></i></a></li>
-                    <li><a href="mailto:carlasantos.educ@gmail.com"><i class="fas fa-envelope"></i></a></li>
-                </ul>
-                <ul class="social-icons">
-                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/leonardocostademoraes/"><i class="fab fa-linkedin"></i></a></li>
-                    <li><a href="mailto:leonardo@senac.com.br"><i class="fas fa-envelope"></i></a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</footer>
+    <h1>motivos</h1>
+
+    <script src="menu.js"></script>
 </body>
 </html>
