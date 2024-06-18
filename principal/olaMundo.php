6407ff1334e66f31f8a309b41712bca28a87c85e
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-06-18 20:56:24 -0300

    UP

--- principal/olaMundo.php
+++ principal/olaMundo.php
@@ -10,9 +10,18 @@
     <h1>Exemplo de PHP</h1>
 
     <?php 
-        date_default_timezone_set("America/Sao_Paulo");
-        echo "Hoje é dia " . date("d/M/Y");
-        echo " No Horario de " . date("G:i:s T");
+        // date_default_timezone_set("America/Sao_Paulo");
+        // echo "Hoje é dia " . date("d/M/Y");
+        // echo " No Horario de " . date("G:i:s T");
+
+        $nome = "Daniel";
+        $sobrenome = "Rodrigues";
+        $idade = "21";
+        $peso = "75";
+        $sobre = "solterio";
+
+        const PAIS = "Brasill";
+        echo "Muito prazer $nome $sobrenome, você mora no " . PAIS . " sua idade é $idade seu peso é $peso estado de relacionamento $sobre";
     ?>
 
 </body>
